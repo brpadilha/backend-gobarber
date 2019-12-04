71840f6db8c32f1ec97515c1c123994c3ccdbbea
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2019-12-04 17:33:17 -0300

    Create User model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'cachorro world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '12345678',
+  });
+  return res.json(user);
 });
 
 export default routes;
