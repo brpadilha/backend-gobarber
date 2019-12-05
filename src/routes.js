42142c0cf23a0dd14137226d0bd1b71effdaa1e8
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2019-12-04 23:22:14 -0300

    Gera um hash da senha

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '12345678',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
