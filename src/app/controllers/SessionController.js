f1b95efa10abb27e6c21ac15c270181d598a87b4
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2019-12-06 21:57:19 -0300

    Gera hash da senha

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(res, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+  }
+}
+
+export default new SessionController();
