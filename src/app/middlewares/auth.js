b82484c5b4ee6ed4d3d19a1083ea184028e47121
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2019-12-09 16:27:31 -0300

    Validando a entrada de dados com Yup

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
-import { verify } from 'crypto';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
