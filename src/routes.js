55246af4439a9f2c5b082ef624c1f3d2ee80dca4
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2019-12-12 11:19:58 -0300

    Configura avatar e lista provedores

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,10 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
+
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,7 +19,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
