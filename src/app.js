8ca752f6e28b62876b5f6a8815269e1982164ada
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2019-12-16 15:02:57 -0300

    Faz alguns ajustes na api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
+import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 import routes from './routes';
@@ -22,6 +23,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
